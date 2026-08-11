6c5af3bcc73782bb236001501ff63ede585f3455
ADDI SATHWIK REDDY <sathwik.addi@bs.nttdata.com>
2026-08-11 06:55:12 +0000

    po list

--- srv/CatalogService.js
+++ srv/CatalogService.js
@@ -21,7 +21,7 @@ module.exports = cds.service.impl(async function () {
         await tx.run(
             UPDATE(POs)
                 .set({
-                    GROSS_AMOUNT: { '+=': 500 },
+                    GROSS_AMOUNT: { '+=': 1500 },
                     NOTE: 'Boosted!!'
                 })
                 .where({ ID })
